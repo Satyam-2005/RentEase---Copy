fd38d3b36c808920b3406801fda7d0a746434e21
Satyam Raj Mahakul <mahakulsatyamraj@gmail.com>
2026-03-14 11:34:41 +0530

    update frontend URL

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin:      process.env.CLIENT_URL || "http://localhost:5173",
+  origin:      process.env.CLIENT_URL || "https://rentease-frontend-qkr6.onrender.com",
   credentials: true,
 }));
 
